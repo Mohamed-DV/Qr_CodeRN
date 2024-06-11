fc0ce616b794ebb1e09f0da90490ffe980414f37
Mohamed-DV <elkhalfimohameddv@gmail.com>
2024-06-11 16:57:16 +0100

    newqrcode

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import Scanner from "./screens/scanner";
+import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import GeneretQrCode from "./screens/generateQrCode";
+const Stack = createStackNavigator();
+function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="Scanner" component={Scanner} />
+      <Stack.Screen name="generetQrCode" component={GeneretQrCode}/>
+      </Stack.Navigator>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default ()=>{
+  return(
+    <NavigationContainer>
+    <App/>
+    </NavigationContainer>
+  )
+}
